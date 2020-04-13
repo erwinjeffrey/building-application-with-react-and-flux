e2fcf9c6ab9dc8e752a5a4b9b5861ba7fc66c3b3
System Administrator <root@Maxs-MacBook-Pro.local>
2020-04-13 17:37:35 -0400

    Input Validation and PropTypes

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TextInput from './common/TextInput';
+import PropTypes from 'prop-types';
 
 const courseForm = props => {
   return (
@@ -10,6 +11,7 @@ const courseForm = props => {
         onChange={props.onChange}
         name="title"
         value={props.course.title}
+        error={props.errors.title}
       />
       <div className="form-group">
         <label htmlFor="author">Author</label>
@@ -26,6 +28,9 @@ const courseForm = props => {
             <option value="2">Scott Allen</option>
           </select>
         </div>
+        {props.errors.authorId && (
+          <div className="alert alert-danger">{props.errors.authorId}</div>
+        )}
       </div>
 
       <TextInput
@@ -34,6 +39,7 @@ const courseForm = props => {
         name="category"
         onChange={props.onChange}
         value={props.course.category}
+        error={props.errors.category}
       />
 
       <input type="submit" value="Save" className="btn btn-primary" />
@@ -41,4 +47,10 @@ const courseForm = props => {
   );
 };
 
+courseForm.propTypes = {
+  course: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
 export default courseForm;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -4,6 +4,7 @@ import * as courseApi from '../api/courseApi';
 import { toast } from 'react-toastify';
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
   const [course, setCourse] = useState({
     id: null,
     slug: '',
@@ -16,8 +17,21 @@ const ManageCoursePage = props => {
     setCourse({ ...course, [target.name]: target.value });
   };
 
+  const formIsValid = () => {
+    const _errors = {};
+
+    if (!course.title) _errors.title = 'Title is required';
+    if (!course.authorId) _errors.authorId = 'AuthorId is required';
+    if (!course.category) _errors.category = 'Category is required';
+
+    setErrors(_errors);
+    //Form is valid if the errors object has no properties
+    return Object.keys(_errors).length === 0;
+  };
+
   const handleSubmit = event => {
     event.preventDefault();
+    if (!formIsValid()) return;
     courseApi.saveCourse(course).then(() => {
       props.history.push('/courses');
       toast.success('Course saved.');
@@ -28,6 +42,7 @@ const ManageCoursePage = props => {
     <>
       <h2>Manage Course</h2>
       <CourseForm
+        errors={errors}
         course={course}
         onChange={handleChange}
         onSubmit={handleSubmit}
